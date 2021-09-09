6a96bc070bc6cbd7a15a578e6354dd5aa9823085
Sean Ferguson <obvioussean@github.com>
2021-09-09 11:18:12 -0700

    Testing no parent label

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # tasks-action
+This simple action automatically labels a task that is created from the "Convert to Issue" feature of the task list on an issue.  
--- index.ts
+++ index.ts
@@ -3,21 +3,25 @@ import * as github from '@actions/github';
 import { GitHub } from '@actions/github/lib/utils';
 
 async function validateParentHasLabel(octokit: InstanceType<typeof GitHub>) {
-    const parentLabel = core.getInput('parent-label', { required: true, trimWhitespace: true });
+    const parentLabel = core.getInput('parent-label', { required: false, trimWhitespace: true });
 
     if (github.context.payload.action === 'opened') {
-        const events = await octokit.rest.issues.listEventsForTimeline({
-            owner: github.context.repo.owner,
-            repo: github.context.repo.repo,
-            issue_number: github.context.payload.issue!.number,
-        });
+        if (parentLabel) {
+            const events = await octokit.rest.issues.listEventsForTimeline({
+                owner: github.context.repo.owner,
+                repo: github.context.repo.repo,
+                issue_number: github.context.payload.issue!.number,
+            });
 
-        const parentIssues = events.data
-            .filter((item) => (item.event === 'cross-referenced' && item.source))
-            .filter((item) => item.source!.issue!.labels
-                .filter((label) => label.name.toLowerCase() === parentLabel.toLowerCase()).length > 0);
+            const parentIssues = events.data
+                .filter((item) => (item.event === 'cross-referenced' && item.source))
+                .filter((item) => item.source!.issue!.labels
+                    .filter((label) => label.name.toLowerCase() === parentLabel.toLowerCase()).length > 0);
 
-        return parentIssues && parentIssues.length > 0;
+            return parentIssues && parentIssues.length > 0;
+        } else {
+            return true;
+        }
     }
 
     core.warning(`Unexpected action ${github.context.payload.action}`);
@@ -28,12 +32,12 @@ async function validateParentHasLabel(octokit: InstanceType<typeof GitHub>) {
 async function labelTask(octokit: InstanceType<typeof GitHub>): Promise<void> {
     const issueNumber = github.context.payload.issue!.number;
     const label = core.getInput('task-label', { required: true, trimWhitespace: true });
-    
+
     await octokit.rest.issues.addLabels({
         owner: github.context.repo.owner,
         repo: github.context.repo.repo,
         issue_number: issueNumber,
-        labels: [ label ]
+        labels: [label]
     });
 
     core.info(`Added label ${label} to ${issueNumber}`);
