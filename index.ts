877aace3563ce8bdcff9038fcc54d96541c5cfe9
Sean Ferguson <obvioussean@github.com>
2021-09-09 09:48:29 -0700

    switch to typescript

--- index.ts
+++ index.ts
@@ -1,19 +1,20 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import { GitHub } from '@actions/github/lib/utils';
 
-async function validateParentHasLabel(octokit) {
+async function validateParentHasLabel(octokit: InstanceType<typeof GitHub>) {
     const parentLabel = core.getInput('parent-label', { required: true, trimWhitespace: true });
 
     if (github.context.payload.action === 'opened') {
-        const events = await octokit.issues.listEventsForTimeline({
+        const events = await octokit.rest.issues.listEventsForTimeline({
             owner: github.context.repo.owner,
             repo: github.context.repo.repo,
-            issue_number: github.context.payload.issue.number,
+            issue_number: github.context.payload.issue!.number,
         });
 
         const parentIssues = events.data
             .filter((item) => (item.event === 'cross-referenced' && item.source))
-            .filter((item) => item.source.issue.labels
+            .filter((item) => item.source!.issue!.labels
                 .filter((label) => label.name.toLowerCase() === parentLabel.toLowerCase()).length > 0);
 
         return parentIssues && parentIssues.length > 0;
@@ -22,10 +23,10 @@ async function validateParentHasLabel(octokit) {
     return false;
 }
 
-async function labelTask(octokit) {
-    const issueNumber = github.context.payload.issue.number;
+async function labelTask(octokit: InstanceType<typeof GitHub>): Promise<void> {
+    const issueNumber = github.context.payload.issue!.number;
     const label = core.getInput('task-label', { required: true, trimWhitespace: true });
-    const result = await octokit.issues.addLabels({
+    const result = await octokit.rest.issues.addLabels({
         owner: github.context.repo.owner,
         repo: github.context.repo.repo,
         issue_number: issueNumber,
@@ -33,11 +34,13 @@ async function labelTask(octokit) {
     });
 }
 
-async function run() {
+async function run(): Promise<void> {
     const token = `${process.env.PAT_TOKEN}`;
-    const octokit = github.getOctokit(token);
+    const octokit = github.getOctokit(token, {
+        previews: ['mockingbird-preview'],
+    });
     const isValidParent = await validateParentHasLabel(octokit);
-    
+
     if (isValidParent) {
         await labelTask(octokit);
     }
